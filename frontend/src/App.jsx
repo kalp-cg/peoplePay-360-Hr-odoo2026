297cb3d110034bcf1bab0839de8315e2d93c85c0
daxp472 <daxpatel.cg@gmail.com>
2026-09-05 16:50:32 +0530

    restored clean horizontal navbar layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,9 +39,12 @@ function ProtectedLayout({ children }) {
   }
 
   return (
-    <Navbar>
-      {children}
-    </Navbar>
+    <div className="min-h-screen flex flex-col bg-[#F8F9FA]">
+      <Navbar />
+      <main className="flex-1 flex flex-col">
+        {children}
+      </main>
+    </div>
   );
 }
 
